a952500c65427c18e55a3601b269c2562778d3a6
tobibot <tobias.heying@outlook.com>
2022-07-06 17:26:15 +0200

    Human readable time

---
+++ HumanReadableTime/Kata.cs
@@ -0,0 +1,21 @@
+﻿
+public static class TimeFormat
+{
+    public static string GetReadableTime(int seconds)
+    {
+
+        // string format(int h, int m, int s) => System.String.Format($"{h.ToString("00")}:{m.ToString("00")}:{s.ToString("00")}", h, m);
+        string format(int h, int m, int s) => System.String.Format("{0:d2}:{1:d2}:{2:d2}", h, m, s);
+
+        if (seconds <= 0 || seconds > 359999)
+            return format(0, 0, 0);        
+
+        var hours = seconds / (60 * 60);
+        seconds -= hours * 60 * 60;
+
+        var minutes = seconds / 60;
+        seconds -= minutes * 60;
+
+        return format(hours, minutes, seconds);        
+    }
+}
---
+++ HumanReadableTime/KataTest.cs
@@ -0,0 +1,18 @@
+using System;
+using NUnit.Framework;
+
+namespace HumanReadableTime;
+
+[TestFixture]
+public class HumanReadableTimeTest
+{
+  [Test]
+  public void HumanReadableTest()
+  {
+       Assert.AreEqual("00:00:00", TimeFormat.GetReadableTime(0));
+       Assert.AreEqual("00:00:05", TimeFormat.GetReadableTime(5));
+       Assert.AreEqual("00:01:00", TimeFormat.GetReadableTime(60));
+       Assert.AreEqual("23:59:59", TimeFormat.GetReadableTime(86399));
+       Assert.AreEqual("99:59:59", TimeFormat.GetReadableTime(359999));
+  }
+}
