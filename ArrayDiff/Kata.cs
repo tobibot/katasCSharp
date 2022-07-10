78932e821b0c674f0c13f0228b3e11b03dd1d854
tobibot <tobias.heying@outlook.com>
2022-07-10 10:51:26 +0200

    ArrayDiff

---
+++ ArrayDiff/Kata.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+  public static int[] ArrayDiff(int[] a, int[] b)
+  {
+    if (a.Length == 0)
+        return a;
+    var c = b.ToList();
+    var deleteFlags = new bool[a.Length];
+
+    for(var i = 0; i < a.Length; i++) 
+    {
+        if (c.Contains(a[i]))
+        {
+            deleteFlags[i] = true;           
+        }
+        
+    }
+
+    var result = new int[a.Length - deleteFlags.Count(d => d)];
+    var ctr = 0;
+    for (var j =  0; j < a.Length; j++)
+    {
+        if (!deleteFlags[j])
+        {
+            result[ctr++] = a[j];
+        }
+    }
+    return result;
+  }
+}
