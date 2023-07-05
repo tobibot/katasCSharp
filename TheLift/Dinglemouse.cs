35338d179ae197340f19ffe5359ddb2b2396f6cb
tobibot <tobias.heying@outlook.com>
2023-07-05 09:28:14 +0200

    sdfdsg

--- TheLift/Dinglemouse.cs
+++ TheLift/Dinglemouse.cs
@@ -1,40 +1,61 @@
 ﻿using System;
 public class Dinglemouse
 {
-    const int up = +1;
-    const int down = -1;
-    static int currentDirection = up;
-    static int currentFloor = 0;
-    static List<int> elevator;
-    static int elevatorCapacity;
-    List<int> visitedFloors = new List<int>();
+    internal const int up = +1;
+    internal const int down = -1;
+    internal static int currentDirection = up;
+    internal static int currentFloor = 0;
+    internal static List<int> elevator;
+    internal static int elevatorCapacity;
+    internal List<int> visitedFloors = new List<int>();
 
     public static int[] TheLift(int[][] queues, int capacity)
     {
         elevatorCapacity = capacity;
         elevator = new List<int>();
+        var maxFloor = queues.Length;
 
         while (!IsWholeQueueEmpty(queues))
         {
-            var floorQueue = queues[currentFloor];
+            var floorQueue = queues[currentFloor].ToList();
             inspectFloorQueue(floorQueue);
-            
+            if (IsWholeQueueEmpty(queues))
+                {
+                    currentFloor = 0;
+                    break;
+                }
+
+            if (currentFloor == 0)            
+                currentDirection = up;            
+            else if (currentFloor == maxFloor)            
+                currentDirection = down;
+            else
+            {
+                if (isNobodyWaitingInDirection(queues, currentDirection))
+                {
+                    currentDirection *= -1;
+                }
+            }
+            currentFloor += currentDirection;            
         }
         
-        for (var i = 0; i < queues.Length; i++)
-        {}
-
-        // Your code here
         return new int[0];
     }
 
-    private static void inspectFloorQueue(int[] floorQueue)
+    private static bool isNobodyWaitingInDirection(int[][] queues, int currentDirection)
     {
-        if (floorQueue.Length == 0)
+        throw new NotImplementedException();
+    }
+
+    private static void inspectFloorQueue(List<int> floorQueue)
+    {
+        if (floorQueue == null || floorQueue.Count == 0)
             return;
+
         // exit
         elevator.Where(e => e == currentFloor).ToList().All(e => elevator.Remove(e));
 
+        // check if all people exited
         var currentCapacity = elevatorCapacity - elevator.Count;
         if (currentCapacity == 0)
             return;
@@ -52,17 +73,29 @@ public class Dinglemouse
                 break;
             default:
                 throw new ArgumentException();
-        }
+        }      
         
-        throw new NotImplementedException();
     }
 
-    private static void letOnePersonIn(int[] floorQueue, List<int> elevator)
+    internal static void letOnePersonIn(List<int> floorQueue, List<int> elevator)
+    {   
+        for (var i = 0; i < floorQueue.Count; i++)
+        {
+            if (floorQueue[i] > currentFloor)
+            {
+                elevator.Add(floorQueue[i]);
+                floorQueue.RemoveAt(i);
+                return;                
+            }
+        }
+    }
+
+    internal static void letOnePersonLeave(List<int> floorQueue, List<int> elevator)
     {
-        throw new NotImplementedException();
+        
     }
 
-    static bool IsWholeQueueEmpty(int[][] queues) =>
+    internal static bool IsWholeQueueEmpty(int[][] queues) =>
         queues.All(q => q == null || q.Length == 0);
     
     
--- TheLift/DinglemouseTest.cs
+++ TheLift/DinglemouseTest.cs
@@ -69,4 +69,53 @@ public class ExampleTests
         var result = Dinglemouse.TheLift(queues, 5);
         Assert.AreEqual(new[] { 0, 5, 4, 3, 2, 1, 0 }, result);
     }
+
+    [Test]
+    public void IsWholeQueueEmpty_QueueEmpty_ReturnsTrue()
+    {
+        int[][] queues =
+        {
+            new int[0], // G
+            new int[0], // 1
+            new int[0], // 2
+            new int[0]  // 3
+        };
+        var result = Dinglemouse.IsWholeQueueEmpty(queues);
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void IsWholeQueueEmpty_QueueNulls_ReturnsTrue()
+    {
+        int[][] queues =
+        {
+            null, // G
+            null, // 1
+            null, // 2
+        };
+        var result = Dinglemouse.IsWholeQueueEmpty(queues);
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void TestLetOnePersonIn_OneSuitableCandidateAndEnoughCapacity_OnePersonIsLetIn()
+    {
+        Dinglemouse.currentFloor = 0;
+        Dinglemouse.currentDirection = Dinglemouse.up;
+        var floorQueue = new List<int>{5, 3, 9};
+        var elevator = new List<int>{8, 4, 3};
+
+        Dinglemouse.letOnePersonIn(floorQueue, elevator);
+
+        Assert.That(elevator, Does.Contain(5));
+        Assert.That(floorQueue, Does.Not.Contain(5));
+        Assert.That(elevator.Count, Is.EqualTo(4));
+        Assert.That(floorQueue.Count, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void IsNobodyWaitingInDirection_NobodyTher_ReturnsTrue()
+    {
+
+    }
 }
